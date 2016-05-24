8450fca7c58ebc4086c9297f9ccc72446e9e89f0
hikomat <nagy.gabor.i@gmail.com>
2016-05-24 13:50:20 +0200

    Added a few comments. Removed functions

--- sample.py
+++ sample.py
@@ -16,6 +16,7 @@ def square(x):
 
 def lin(params):
     pid = os.getpid()
+    print("Parameters: {}. (PID={})".format(params, pid))
     a = params.get('a')
     b = params.get('b')
     x = params.get('x')
@@ -62,7 +63,7 @@ def get_data(params):
 
 def execute_on_process_pool(function, arguments, max_workers=None):
     t0 = time.time()
-    print(21 * "-")
+    print(42 * "-")
     print('Process Pool Executor')
     print('Function to execute: {}'.format(function))
     print('Max workers: {}'.format(max_workers))
@@ -77,7 +78,7 @@ def execute_on_process_pool(function, arguments, max_workers=None):
 
 def execute_on_thread_pool(function, arguments, max_workers=1):
     t0 = time.time()
-    print(21 * "-")
+    print(42 * "-")
     print('Thread Pool Executor')
     print('Function to execute: {}'.format(function))
     print('Max threads: {}'.format(max_workers))
@@ -89,14 +90,6 @@ def execute_on_thread_pool(function, arguments, max_workers=1):
     return results
 
 
-def get_historical_prices(symbol_list):
-    arguments = []
-    for symbol in symbol_list:
-        arguments.append({'start_date': '2015-01-01', 'end_date': '2016-01-01', 'symbol': symbol})
-    results = execute_on_thread_pool(get_data, arguments, max_workers=8)
-    return list(filter(lambda x: x is not None, results))
-
-
 def quotes(symbols):
     s = '+'.join(symbols)
     url = 'http://finance.yahoo.com/d/quotes.csv?s={}&f=snabopghc1l1'.format(s)
@@ -104,26 +97,14 @@ def quotes(symbols):
     return result.text
 
 
-def compute_stats(dataframes):
-    results = execute_on_thread_pool(stats, dataframes, max_workers=8)
-    return pd.DataFrame(results)
-
-
 def stats(d):
     r = d.adjclose / d.adjclose.shift(1)
     return pd.Series({
+        'r': r.prod(),
         'r_std': r.std(),
         'r_mean': r.mean(),
         'symbol': d.symbol.iloc[0]
     })
-
-
-def correlate_returns(dataframes):
-    c = combinations(dataframes, 2)
-    correlation_series = execute_on_process_pool(corr, c, max_workers=8)
-    correlation_df = pd.DataFrame(correlation_series)
-    return correlation_df
-    # return pd.pivot_table(correlation_df, 'r', 's1', 's2', fill_value=None)
 
 
 def corr(combination):
@@ -141,6 +122,7 @@ if __name__ == '__main__':
 
     arguments = range(8)
     execute_on_process_pool(square, arguments)
+    execute_on_thread_pool(square, arguments, max_workers=8)
 
     arguments = [
         {'a': 1, 'b': 2, 'x': 4},
@@ -155,13 +137,63 @@ if __name__ == '__main__':
     # Randomize
     np.random.shuffle(symbols)
 
-    # Download data
-    dataframes = get_historical_prices(symbols[0:128])
+    # Arguments for the downloader
+    arguments = []
+    for symbol in symbols[0:64]:
+        arguments.append({'start_date': '2015-01-01', 'end_date': '2016-01-01', 'symbol': symbol})
+
+    dataframes = execute_on_thread_pool(get_data, arguments, max_workers=8)
+    dataframes = execute_on_process_pool(get_data, arguments, max_workers=8)
+
+    print(42 * '-')
+    print("Single thread")
+    t0 = time.time()
+    dataframes = []
+    for arg in arguments:
+        dataframes.append(get_data(arg))
+    print("Elapsed: {:.4f}".format(time.time() - t0))
+
+    dataframes = list(filter(lambda x: x is not None, dataframes))
+    print()
+    print(42 * '-')
     print("Downloaded {}".format(len(dataframes)))
 
     # Compute stats with thread
-    returns_and_sds = compute_stats(dataframes)
-    print(returns_and_sds)
+    return_stats = execute_on_process_pool(stats, dataframes, max_workers=8)
+    return_stats = pd.DataFrame(return_stats)
+    print(return_stats.shape)
 
-    c = correlate_returns(dataframes)
-    print(c)
+    return_stats = execute_on_thread_pool(stats, dataframes, max_workers=8)
+    return_stats = pd.DataFrame(return_stats)
+    print(return_stats.shape)
+
+    combs = combinations(dataframes, 2)
+    correlation_series = execute_on_process_pool(corr, combs, max_workers=8)
+    correlation_df = pd.DataFrame(correlation_series)
+    print(correlation_df.shape)
+
+    combs = combinations(dataframes, 2)
+    correlation_series = execute_on_thread_pool(corr, combs, max_workers=8)
+    correlation_df = pd.DataFrame(correlation_series)
+    print(correlation_df.shape)
+
+    combs = combinations(dataframes, 2)
+    correlation_series = execute_on_thread_pool(corr, combs, max_workers=4)
+    correlation_df = pd.DataFrame(correlation_series)
+    print(correlation_df.shape)
+
+    combs = combinations(dataframes, 2)
+    correlation_series = execute_on_thread_pool(corr, combs, max_workers=2)
+    correlation_df = pd.DataFrame(correlation_series)
+    print(correlation_df.shape)
+
+    combs = combinations(dataframes, 2)
+    t0 = time.time()
+    correlation_series = []
+    for d1, d2 in combs:
+        correlation_series.append(corr((d1, d2)))
+    print("Elapsed: {:.4f}".format(time.time() - t0))
+
+    correlation_df = pd.DataFrame(correlation_series)
+    print(correlation_df.shape)
+
